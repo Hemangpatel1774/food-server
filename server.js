775c511b94d636081ad16d5c4ef9d0dda69fda1a
Hemang1774 <patelhemang1774@gmail.com>
2024-12-24 14:51:23 +0530

    env changing

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const mongoose = require('mongoose');
 dotenv.config();        
 const port = process.env.PORT || 3000;
 
+
+
 // mongoose.connect(process.env.DBURL)
 // .then(() => console.log("DB Connected..!")) 
+
+app.get('/', (req, res) => {
+    res.send(process.env.DBURL);
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
