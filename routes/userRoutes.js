c94995bd1a7fa315a7b71057bf914a407de79e5f
Hemang1774 <patelhemang1774@gmail.com>
2024-12-24 17:59:00 +0530

    testing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,35 +4,45 @@ const User = require('../Models/userModel');
 
 userRouter.post('/registerUser', async (req, res) => {
     try {
-        const data = {
-            name: "John Doe",
-            email: "johndoe@example.com",
-            password: "password123",
-            cart: [
-                {
-                    foodName: "Pizza",
-                    foodQuantity: 2,
-                    foodPrice: 15.99,
-                    foodImage: Buffer.from(""),
-                }
-            ],
-            history: [
-                {
-                    foodName: "Burger",
-                    foodQuantity: 1,
-                    foodPrice: 9.99,
-                    foodImage: Buffer.from(""),
-                    date: new Date()
-                }
-            ],
-            date: new Date()
-        };
-        const users = await User(data);
-        await users.save();
-        res.json(users);
+        const data = req.body;
+        res.send(data);
+        // const users = await User(data);
+        // await users.save();
+        // res.json(users);
     } catch (err) {
         res.json({ message: err });
     }
 });
 
 module.exports = userRouter;
+
+
+
+
+
+
+
+
+
+// {
+//     "name": "John Doe",
+//     "email": "johndoe@example.com",
+//     "password": "password123",
+//     "cart": [
+//         {
+//             "foodName": "Pizza",
+//             "foodQuantity": 2,
+//             "foodPrice": 15.99,
+//             "foodImage": Buffer.from(""),
+//         }
+//     ],
+//     "history": [
+//         {
+//             "foodName": "Burger",
+//             "foodQuantity": 1,
+//             "foodPrice": 9.99,
+//             "foodImage": Buffer.from(""),
+//             "date": new Date()
+//         }
+//     ],
+// };
