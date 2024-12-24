eaebf00a25a45019b76658607357d6f9ff93cbc5
Hemang1774 <patelhemang1774@gmail.com>
2024-12-24 13:00:37 +0530

    change vercel.json

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.DBURL)
-.then(() => console.log("DB Connected..!")) 
+// mongoose.connect(process.env.DBURL)
+// .then(() => console.log("DB Connected..!")) 
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
