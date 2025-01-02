a84fd171f2016ba2b838a5aa6379be794b320e43
Hemang1774 <patelhemang1774@gmail.com>
2025-01-02 12:05:01 +0530

    changes in res.send() put message:'ok'

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/userRoutes');
